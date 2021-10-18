fc7b11aac06123c32f5787eedf1986eb19d4bc2c
Gavin Murphy <gavinhmurphy@gmail.com>
2021-10-17 21:15:44 -0700

    template questions

---
+++ logic.js
@@ -0,0 +1,30 @@
+// create three different prompts
+// create a card template for each of the worker type
+// place cards into template html with function
+
+const internQuestions = [
+    {
+      name: 'name',
+      type: 'input',
+      message: 'What is the intern\'s name?',
+      required: 'true'
+    },
+    {
+      name: 'id',
+      type: 'input',
+      message: 'What is the intern\'s employee ID?',
+      required: 'true'
+    },
+    {
+      name: 'email',
+      type: 'input',
+      message: 'What is the intern\'s email address?',
+      required: 'true'
+    },
+    {
+      name: 'school',
+      type: 'input',
+      message: 'What is the intern\'s school?',
+      required: 'true'
+    },
+  ]
